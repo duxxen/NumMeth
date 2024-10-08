8ff6b0ad63f5ed8cc7192d086869fbab20b491fd
duxxen <a.romanovu@gmail.com>
2024-10-08 06:13:05 +0300

    .csv output renamed

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -277,7 +277,7 @@ int lab1()
 	cout << "alpha_p = \n" << alpha6 << endl;
 	cout << "-------------------------\n";
 
-	std::ofstream fout("lab1/abs_alpha.csv");
+	std::ofstream fout("lab1/lab1_output.csv");
 	if (!fout.is_open())
 	{
 		cout << "Error occuried while opening file!\n";
